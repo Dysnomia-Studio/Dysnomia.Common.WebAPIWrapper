32586c6793d6ae5a093df44c60001c255de0033e
Elanis <elanis@hotmail.com>
2021-06-24 19:23:10 +0200

    Added NumberToBooleanConverter

---
+++ Dysnomia.Common.WebAPIWrapper/Helpers/NumberToBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Dysnomia.Common.WebAPIWrapper.Helpers {
+	public class NumberToBooleanConverter : JsonConverter<bool> {
+		public override bool Read(ref Utf8JsonReader reader, Type type, JsonSerializerOptions options) {
+			if (reader.TokenType == JsonTokenType.Number) {
+				return reader.GetInt64() == 1;
+			}
+
+			return reader.GetBoolean();
+		}
+		public override bool CanConvert(Type typeToConvert) {
+			return true;
+		}
+
+		public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options) {
+			JsonSerializer.Serialize(writer, value, options);
+		}
+	}
+}
