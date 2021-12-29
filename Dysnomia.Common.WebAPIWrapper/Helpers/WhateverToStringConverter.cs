6e206dbb314e2dda548a59971f182f23d08f27a8
Elanis <elanis@hotmail.com>
2021-12-29 19:54:29 +0100

    Woopsie, typo

--- Dysnomia.Common.WebAPIWrapper/Helpers/WhateverToStringConverter.cs
+++ Dysnomia.Common.WebAPIWrapper/Helpers/WhateverToStringConverter.cs
@@ -6,7 +6,7 @@ namespace Dysnomia.Common.WebAPIWrapper.Helpers {
 	public class WhateverToStringConverter : JsonConverter<string> {
 		public override string Read(ref Utf8JsonReader reader, Type type, JsonSerializerOptions options) {
 			if (reader.TokenType == JsonTokenType.Number) {
-				return reader.GetUInt64().ToString();
+				return reader.GetInt64().ToString();
 			}
 
 			if (reader.TokenType == JsonTokenType.String) {
