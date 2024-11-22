ca75651da1fa9809b1faef5f8952754311f6ed9d
Elanis <elanis@hotmail.com>
2024-11-22 19:10:28 +0100

    Simplify Activator.CreateInstance call in EmptyArrayToObjectConverter

--- Dysnomia.Common.WebAPIWrapper/Helpers/EmptyArrayToObjectConverter.cs
+++ Dysnomia.Common.WebAPIWrapper/Helpers/EmptyArrayToObjectConverter.cs
@@ -3,26 +3,26 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Dysnomia.Common.WebAPIWrapper.Helpers {
-	public class EmptyArrayToObjectConverter<T> : JsonConverter<T> {
-		public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
-			var rootElement = JsonDocument.ParseValue(ref reader);
+    public class EmptyArrayToObjectConverter<T> : JsonConverter<T> {
+        public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+            var rootElement = JsonDocument.ParseValue(ref reader);
 
-			// if its array return new instance or null
-			if (reader.TokenType == JsonTokenType.EndArray) {
-				// return default(T); // if you want null value instead of new instance
-				return (T)Activator.CreateInstance(typeof(T));
-			} else {
-				var text = rootElement.RootElement.GetRawText();
-				return JsonSerializer.Deserialize<T>(text, options);
-			}
-		}
+            // if its array return new instance or null
+            if (reader.TokenType == JsonTokenType.EndArray) {
+                // return default(T); // if you want null value instead of new instance
+                return Activator.CreateInstance<T>();
+            } else {
+                var text = rootElement.RootElement.GetRawText();
+                return JsonSerializer.Deserialize<T>(text, options);
+            }
+        }
 
-		public override bool CanConvert(Type typeToConvert) {
-			return true;
-		}
+        public override bool CanConvert(Type typeToConvert) {
+            return true;
+        }
 
-		public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options) {
-			JsonSerializer.Serialize(writer, value, options);
-		}
-	}
+        public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options) {
+            JsonSerializer.Serialize(writer, value, options);
+        }
+    }
 }
